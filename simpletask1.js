0a8f82c4213458fc9b974dc1c6949d249d198ae3
Madhuz02 <mahalakshmirl02@gmail.com>
2025-07-16 20:54:33 +0530

    Add files via upload

---
+++ simpletask1.js
@@ -0,0 +1,18 @@
+
+var age = 25;
+console.log("Age using var:", age);
+
+
+var age = 30;
+console.log("Updated age using var:", age);
+
+let name = "Madhu";
+console.log("Name using let:", name);
+
+name = "Rohith";
+console.log("Updated name using let:", name);
+
+const country = "India";
+console.log("Country using const:", country);
+
+
